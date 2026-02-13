95cfd9f23609133902450002410cc843fafe0c44
benRenard <benjamin.renard@inrae.fr>
2026-02-13 17:42:34 +0100

    Preliminary implementation of the plotting function for object recursiveModeling.

--- R/class_recursiveModeling.R
+++ R/class_recursiveModeling.R
@@ -61,3 +61,98 @@ validate_recursiveModeling<-function(x){
   # nothing to do
   return(x)
 }
+
+#***************************************************************************----
+# plotting ----
+
+plot_recursiveModeling_data <- function(x,type=c('xy','ty','tx')){
+  typ=match.arg(type)
+  if(typ=='xy'){
+    xlab='x';ylab='y'
+  } else if (typ=='ty'){
+    xlab='Time';ylab='y'
+  } else if (typ=='tx'){
+    xlab='Time';ylab='x'
+  } else {
+    stop('Unknown plot type')
+  }
+  terminal=x$segmentation$tree$indx[x$segmentation$tree$nS==1]
+  DF=c()
+  for(i in 1:length(terminal)){
+    node=terminal[i]
+    foo=x$fit[[node]]$data
+    nX=NCOL(foo)-5
+    if(nX==1){
+      names(foo)[2]='x'
+      DF=rbind(DF,cbind(foo,period=i))
+    } else if(nX >1) {
+
+    } else {
+
+    }
+  }
+  DF$period=as.factor(DF$period)
+  # 2DO: review fitModel object to store original data in a standardized way, including uY/uX,
+  # hence allowing plotting error bars
+  # 2DO: handle multi-X case
+  DF$uY=0
+  colourCount_obs=length(unique(DF$period))
+  getPalette_obs=scales::viridis_pal(option='D')
+  if(typ=='xy'){
+    g=ggplot(DF,aes(x=x))
+  } else {
+    g=ggplot(DF,aes(x=time))
+  }
+  if(typ=='tx'){
+    g=g+geom_point(aes(y=x,group=period,color=period))
+  } else {
+    g=g+geom_point(aes(y=obs,group=period,color=period))
+    if(any(DF$uY>0)){
+      g=g+geom_errorbar(aes(y=.data$obs,
+                            ymin=.data$obs-1.96*.data$uY,
+                            ymax=.data$obs+1.96*.data$uY,
+                            color=.data$period,group=.data$period))
+    }
+  }
+  if(typ=='xy'){
+    g=g+geom_line(aes(y=sim,group=period,color=period))
+  }
+  g=g+scale_color_manual(values=getPalette_obs(colourCount_obs))
+  g=g+labs(y=ylab,x=xlab,title='Segmented data')
+  g=g+theme_bw()+theme(plot.title=element_text(hjust=0.5,face='bold',size=15),
+                       legend.position="none")
+  return(g)
+}
+
+#' recursiveModeling plotter
+#'
+#' Plot a recursiveModeling object.
+#' @param x object of class recursiveModeling
+#' @param type string, type of plot
+#' @param dataPlotType string, type of data plot: 'xy' for a x-y scatterplot,
+#'     'ty' for a time series plot of y, 'tx' for a time series plot of x
+#' @param ... Optional arguments
+#' @return a ggplot
+#' @examples
+#' x=Segmentation_RecursiveModeling(x=ArdecheRiverGaugings$H,y=ArdecheRiverGaugings$Q)
+#' plot(x)
+#' @export
+plot.recursiveModeling <- function(x,type=c('both','data','shifts'),dataPlotType=c('xy','ty','tx'),...){
+  typ=match.arg(type)
+  dtyp=match.arg(dataPlotType)
+  if(typ=='data'){
+    g=plot_recursiveModeling_data(x,type=dtyp)
+  } else if(typ=='shifts'){
+    g=plot_shifts(x$segmentation)
+  } else if(typ=='both'){
+    g1=plot_recursiveModeling_data(x,type=dtyp)
+    g2=plot_shifts(x$segmentation)
+    g=wrap_plots(g1+labs(title='Segmented data and shifts'),
+                 g2+labs(title=NULL),ncol=1)+
+      plot_layout(axes='collect_x')
+  } else {
+    g=NULL
+  }
+  return(g)
+}
+
